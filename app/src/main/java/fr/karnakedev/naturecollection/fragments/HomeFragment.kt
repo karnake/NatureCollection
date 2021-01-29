d0e33b80ff21d5d23e0b835731e1c76a4e5a5fca
Elies <elieskedim1@gmail.com>
2021-01-29 23:11:14 +0100

    inject component HomeFragment in main activity

---
+++ app/src/main/java/fr/karnakedev/naturecollection/fragments/HomeFragment.kt
@@ -0,0 +1,15 @@
+package fr.karnakedev.naturecollection.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import fr.karnakedev.naturecollection.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_home, container, false)
+    }
+}
